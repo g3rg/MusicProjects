0fa354dfaa22dc70abeccbe266652b65f2b6912a
G3rg <mauve.simian@gmail.com>
2023-04-18 14:51:06 +1000

    Add some notes

--- Rc1FootSwitch.py
+++ Rc1FootSwitch.py
@@ -2,6 +2,12 @@ import board
 import digitalio
 from adafruit_debouncer import Debouncer
 
+# 2 x Foot switches, or 3? Stop, Delete, Undo
+# Can a relay be used to switch audio socket as well? Would SPST work there,
+# or would that leave floating ground interference yadda yadda?
+#
+
+
 pin = digitalio.DigitalInOut(board.D12)
 pin.direction = digitalio.Direction.INPUT
 pin.pull = digitalio.Pull.UP
